59726eccf11cea8f121601b3f58f3b21a7687547
tysonv7 <tyson_v7@hotmail.com>
2016-04-02 19:26:37 +0800

    Added the headings of each row in the GUI

--- LTE Sniffer/software/main.py
+++ LTE Sniffer/software/main.py
@@ -1,12 +1,28 @@
 import kivy
-kivy.require('1.9.1')
-
+from kivy.uix.gridlayout import GridLayout
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.button import Button
 
-class MyApp(App):
+    
+class LTE_Sniffer(App):
+    
+    def update(self):
+        for x in range (0,140):
+            layout.add_widget(Button(text='test'))
+            return layout
+            
     def build(self):
-        return Label(text='Hello world')
+        layout = GridLayout(cols=7, row_force_default=True, row_default_height=40)
+        layout.add_widget(Button(text='MCC'))
+        layout.add_widget(Button(text='MNC'))
+        layout.add_widget(Button(text='Carrier'))
+        layout.add_widget(Button(text='Frequency'))
+        layout.add_widget(Button(text='Physical Cell ID'))
+        layout.add_widget(Button(text='PSS Power'))
+        layout.add_widget(Button(text='Last Seen Due'))
+        LTE_Sniffer.update()
+        return layout
     
 if __name__ == '__main__':
-    MyApp().run()
+    LTE_Sniffer().run()
+        
