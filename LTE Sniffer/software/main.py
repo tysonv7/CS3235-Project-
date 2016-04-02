200da27f96e441cb6716c9f63a8dc5d73e79296e
tysonv7 <tyson_v7@hotmail.com>
2016-04-02 16:58:13 +0800

    Added LTE Sniffer GUI skeleton code

---
+++ LTE Sniffer/software/main.py
@@ -0,0 +1,12 @@
+import kivy
+kivy.require('1.9.1')
+
+from kivy.app import App
+from kivy.uix.label import Label
+
+class MyApp(App):
+    def build(self):
+        return Label(text='Hello world')
+    
+if __name__ == '__main__':
+    MyApp().run()
