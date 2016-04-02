38d68801d5c4a7aee1ca202d37fd529a65a589c5
tysonv7 <tyson_v7@hotmail.com>
2016-04-02 19:56:58 +0800

    Finally able to have all the rows and columns in the GUI

--- LTE Sniffer/software/main.py
+++ LTE Sniffer/software/main.py
@@ -5,11 +5,6 @@ from kivy.uix.button import Button
 
     
 class LTE_Sniffer(App):
-    
-    def update(self):
-        for x in range (0,140):
-            layout.add_widget(Button(text='test'))
-            return layout
             
     def build(self):
         layout = GridLayout(cols=7, row_force_default=True, row_default_height=40)
@@ -20,9 +15,11 @@ class LTE_Sniffer(App):
         layout.add_widget(Button(text='Physical Cell ID'))
         layout.add_widget(Button(text='PSS Power'))
         layout.add_widget(Button(text='Last Seen Due'))
-        LTE_Sniffer.update()
+
+        for x in range (0,140):
+            layout.add_widget(Button(text='test'))
         return layout
     
 if __name__ == '__main__':
     LTE_Sniffer().run()
-        
+            
