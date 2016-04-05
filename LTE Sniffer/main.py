2faa0494492fdf2cf625a5b734a82256343a93e9
tysonv7 <tyson_v7@hotmail.com>
2016-04-05 13:12:56 +0800

    GUI can link with database and can refresh data upon request

--- LTE Sniffer/main.py
+++ LTE Sniffer/main.py
@@ -1,5 +1,14 @@
+#!/usr/bin/env python
 import kivy
+import mysql.connector
+import subprocess
+import sys
+import os
 from kivy.config import Config
+Config.set('kivy', 'desktop', 1)
+Config.write()
+Config.set('kivy', 'exit_on_escape', 1)
+Config.write()
 Config.set('graphics', 'width', '850')
 Config.set('graphics', 'height', '840')
 from kivy.uix.gridlayout import GridLayout
@@ -12,22 +21,26 @@ from kivy.core.window import Window
 class LTE_Sniffer(App):
             
     def build(self):
+        self.db = mysql.connector.connect(host="localhost",user="root",passwd="ilovegeniussnorlax",db='LTESnifferDB')
+        self.cur = self.db.cursor()
+        self.cur.execute("SELECT * FROM LTESnifferTable;")
         self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
         self._keyboard.bind(on_key_down=self._on_keyboard_down)
         self.layout = GridLayout(cols=7, row_force_default=True, row_default_height=40)
-        self.layout.add_widget(Button(text='Frequency'))
+        self.layout.add_widget(Button(text='Frequency\MHz'))
         self.layout.add_widget(Button(text='MCC'))
         self.layout.add_widget(Button(text='MNC'))
         self.layout.add_widget(Button(text='Carrier'))
         self.layout.add_widget(Button(text='Physical Cell ID'))
-        self.layout.add_widget(Button(text='PSS Power'))
-        self.layout.add_widget(Button(text='Last Seen Due'))
+        self.layout.add_widget(Button(text='PSS Power\dBm'))
+        self.layout.add_widget(Button(text='Last Seen Day'))
         self.update()
         return self.layout
 
     def update(self):
-        for x in range (0,140):
-            self.layout.add_widget(Button(text=str(x)))
+        for row in self.cur.fetchall():
+            for col in range (0,7):
+                self.layout.add_widget(Button(text=str(row[col])))
 
     def _keyboard_closed(self):
         self._keyboard.unbind(on_key_down=self._on_keyboard_down)
@@ -35,38 +48,9 @@ class LTE_Sniffer(App):
 
     def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
         if keycode[1] == 'u':
-            print("test test")
+            os.execl(sys.executable, sys.executable, *sys.argv)
+##            subprocess.call['./FileToCSv.sh']
         return True
     
 if __name__ == '__main__':
     LTE_Sniffer().run()
-
-##class Grid(Widget):
-##    def prepare_grid(self):
-##        self.layout = GridLayout(cols=7, row_force_default=True, row_default_height=40)
-##        self.layout.add_widget(Button(text='MCC'))
-##        self.layout.add_widget(Button(text='MNC'))
-##        self.layout.add_widget(Button(text='Carrier'))
-##        self.layout.add_widget(Button(text='Frequency'))
-##        self.layout.add_widget(Button(text='Physical Cell ID'))
-##        self.layout.add_widget(Button(text='PSS Power'))
-##        self.layout.add_widget(Button(text='Last Seen Due'))
-##        self.update()
-##        return self.layout
-##
-##    def update(self):
-##      for x in range (0,140):
-##            self.layout.add_widget(Button(text='test'))
-##        
-##            
-##class LTE_Sniffer(App):
-##
-##    def build(self):
-##        table = Grid()
-##        table.prepare_grid()
-##        return table
-##
-##if __name__ == '__main__':
-##    LTE_Sniffer().run()
-        
-        
